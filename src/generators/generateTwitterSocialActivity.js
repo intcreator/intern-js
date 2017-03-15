a8b8478830162cfa13316f5daf6a477d4bd17ed2
Brandon Blatter <intcreator@users.noreply.github.com>
2017-03-15 16:10:31 -0600

    Finish consumer tests

--- src/generators/generateTwitterSocialActivity.js
+++ src/generators/generateTwitterSocialActivity.js
@@ -1,3 +1,27 @@
-export default function () {
-  return {};
+import _ from 'lodash';
+
+export default function twitterTransformer (message) {
+  const PostMedia = [];
+  if (message.images && message.images.standard_resolution) {
+    PostMedia.push({
+      MediaType: _.get(message, 'type') || 'image',
+      URL: message.images.standard_resolution.url,
+    });
+  }
+  const socialActivity = {
+    ActivityURL: _.get(message, 'link'),
+    AuthorID: _.get(message, 'user.id'),
+    AuthorPictureURL: _.get(message, 'user.profile_picture'),
+    AuthorRealName: _.get(message, 'user.full_name'),
+    AuthorUsername: _.get(message, 'user.username'),
+    BodyText: _.get(message, 'body'),
+    ID: message.id,
+    Network: 'twitter',
+    PostMedia,
+    Version: '2.0',
+  };
+  if (socialActivity.ID && socialActivity.BodyText) {
+    return socialActivity;
+  }
+  return undefined;
 }
